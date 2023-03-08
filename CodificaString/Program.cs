51ca83c67ceddddeb54bf8dd3ab5456e52d9020b
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-07 21:28:00 -0300

    Add project files.

---
+++ CodificaString/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string texto;
+        char codigo;
+
+        Console.Write("Digite uma frase para ser codificada: ");
+        texto = Console.ReadLine();
+
+        Console.Write("Digite um caractere para a codificação: ");
+        codigo = char.Parse(Console.ReadLine());
+
+        Console.Write("Texto codificado: ");
+        foreach (char c in codificaTexto(texto, codigo))
+        {
+            Console.Write(c);
+        }
+
+        List<char> codificaTexto(string texto, char codigo)
+        {
+            List<char> textoCodificado = new();
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (texto[i] != 'a' && texto[i] != 'e' && texto[i] != 'i' && texto[i] != 'o' && texto[i] != 'u' && !char.IsWhiteSpace(texto[i]))
+                {
+                    textoCodificado.Add(codigo);
+                    textoCodificado.Add(texto[i]);
+                }
+                else
+                {
+                    textoCodificado.Add(texto[i]);
+                }
+            }
+            return textoCodificado;
+        }
+
+    }
+}
